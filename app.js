aab3505a4f02bd76960fb72eefbfb436e5f2f7f9
DominikFrackowiak <rorschach@op.pl>
2024-02-20 12:15:18 +0100

    updates

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { connectToDb, getDb } = require('./db')
 // init app / middleware
 
 const app = express()
+app.use(express.json())
 
 // db connection
 let db
@@ -22,15 +23,59 @@ app.get('/api/music-reviews', (req, res) => {
 	db.collection('reviews')
 		.find() //'cursor' object not data per se, toArray / for each
 		.sort({ band: 1 })
-		.forEach(record => {
-			console.log(record)
-			records.push(record)
-		})
-		.then(console.log(records))
+		.forEach(record => records.push(record))
+
 		.then(() => {
 			res.status(200).json(records)
 		})
 		.catch(() => {
 			res.status(500).json({ msg: 'Could not fetch the data' })
+		})
+})
+
+app.get('/api/music-reviews/:slug', (req, res) => {
+	const slug = req.params.slug
+
+	db.collection('reviews')
+		.findOne({ slug })
+		.then(doc => res.status(200).json(doc))
+		.catch(err =>
+			res.status(500).json({ error: 'Could not fetch the document' })
+		)
+})
+
+app.post('/api/music-reviews', (req, res) => {
+	const record = req.body
+
+	db.collection('reviews')
+		.insertOne(record)
+		.then(result => {
+			res.status(201).json(result)
+		})
+		.catch(err => {
+			res.status(500).json({ err: 'Could not create new document' })
+		})
+})
+
+app.delete('/api/music-reviews/:slug', (req, res) => {
+	const slug = req.params.slug
+
+	db.collection('reviews')
+		.deleteOne({ slug })
+		.then(doc => res.status(200).json(doc))
+		.catch(err => res.status(500).json({ err: 'Could not delete the document' }))
+})
+
+app.patch('/api/music-reviews/:slug', (req, res) => {
+	const updates = req.body
+	const slug = req.params.slug
+
+	db.collection('reviews')
+		.updateOne({ slug }, { $set: updates })
+		.then(result => {
+			res.status(201).json(result)
+		})
+		.catch(err => {
+			res.status(500).json({ err: 'Could not update new document' })
 		})
 })
